b838b5e9153fadb510ce839e7d59996817755440
Malinza <vimalrathod@vvsdtz.com>
2023-06-06 17:10:35 +0300

    created staff loan settings Updated workspace

---
+++ staff_loans/staff_loan_management/doctype/staff_loan_settings/staff_loan_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, VV System Developers LTD and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Staff Loan Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
