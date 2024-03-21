9fc6da4b869cd46d067addb736a8f6e3662551dc
Ibrahim Malinza <ibrahim@vvsdtz.com>
2024-03-21 10:42:03 +0300

    feat: update workspace

--- staff_loans/__init__.py
+++ staff_loans/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = '15.0.6'
+__version__ = '15.0.7'
 
 from hrms.payroll.doctype.salary_slip import salary_slip_loan_utils
 from staff_loans.Custom.button_method import custom_set_loan_repayment
