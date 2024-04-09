6f768e0c3dd14c60f538df8f51e8e7e9b99b40f5
Ibrahim Malinza <ibrahim@vvsdtz.com>
2024-04-09 15:53:52 +0300

    feat: add staff loan agreement and fetch account from mode of payment

--- staff_loans/staff_loan_management/doctype/staff_loan_type/staff_loan_type.py
+++ staff_loans/staff_loan_management/doctype/staff_loan_type/staff_loan_type.py
@@ -1,8 +1,16 @@
 # Copyright (c) 2024, VV System Developers LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class StaffLoanType(Document):
 	pass
+
+@frappe.whitelist()
+def get_mode_of_payment_account(company,mode_of_payment):
+	doc = frappe.get_doc("Mode of Payment",mode_of_payment)
+	if len(doc.accounts) > 0:
+		for row in doc.accounts:
+			if row.company == company and row.default_account:
+				return row.default_account
